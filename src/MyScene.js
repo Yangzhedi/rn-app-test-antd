824c9be8543d52b743b9a61181fac882462969e3
Yangzhedi <uiryzd@163.com>
2017-07-12 00:10:11 +0800

    init

---
+++ src/MyScene.js
@@ -0,0 +1,24 @@
+import React, { Component,PropTypes  } from 'react';
+import { View, Text,TouchableHighlight  } from 'react-native';
+
+export default class MyScene extends Component {
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		onForward: PropTypes.func.isRequired,
+		onBack: PropTypes.func.isRequired,
+	};
+
+	render() {
+		return (
+			<View>
+				<Text>Current Scene: { this.props.title }</Text>
+				<TouchableHighlight onPress={this.props.onForward}>
+					<Text>点我进入下一场景321</Text>
+				</TouchableHighlight>
+				<TouchableHighlight onPress={this.props.onBack}>
+					<Text>点我返回上一场景123</Text>
+				</TouchableHighlight>
+			</View>
+		)
+	}
+}
